6e25c7e18e5faea53ec8c69fe13e996d69326977
Maxence BOCQUEL <mbocquel@e1r9p3.clusters.42paris.fr>
2022-12-08 23:47:33 +0100

    sauv du 8 dec bis

--- Fdf/my_fdf/srcs/free_functions.d
+++
@@ -1,8 +0,0 @@
-srcs/free_functions.o: srcs/free_functions.c srcs/fdf.h mlx_linux/mlx.h \
-  srcs/../libft/libft.h
-
-srcs/fdf.h:
-
-mlx_linux/mlx.h:
-
-srcs/../libft/libft.h:
--- Fdf/my_fdf/srcs/parsing.d
+++
@@ -1,8 +0,0 @@
-srcs/parsing.o: srcs/parsing.c srcs/fdf.h mlx_linux/mlx.h \
-  srcs/../libft/libft.h
-
-srcs/fdf.h:
-
-mlx_linux/mlx.h:
-
-srcs/../libft/libft.h:
