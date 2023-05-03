578445b391e1a05696c29d358e452cfe0b850199
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2023-05-03 16:18:46 +0200

    remove .o

--- minishell/build/exec/init.d
+++
@@ -1,9 +0,0 @@
-build/exec/init.o: src/exec/init.c src/exec/../include/minishell.h \
-  src/exec/../include/../../libft/src/libft.h \
-  src/exec/../include/./minishell_struct.h
-
-src/exec/../include/minishell.h:
-
-src/exec/../include/../../libft/src/libft.h:
-
-src/exec/../include/./minishell_struct.h:
